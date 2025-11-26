508b6bc64abdb0d246d310a6990b30098b6a7395
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2025-11-26 03:08:17 -0600

    Add Rybbit analytics

---
+++ rybbit-analytics.js
@@ -0,0 +1,8 @@
+(function() {
+  const script = document.createElement('script');
+  script.src = 'https://applytics.browser.cash/api/script.js';
+  script.async = true;
+  script.defer = true;
+  script.setAttribute('data-site-id', 'eb7067872860');
+  document.head.appendChild(script);
+})();
